5aa947d7c38462bd32a6b5a390a05117d7b9e920
matttm <matttmaloney@gmail.com>
2024-03-10 20:23:57 -0400

    working on main

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,14 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok.body(req_body)
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Response<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(echo)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
